1903775232c5517e32d1592b5dee45f34ffffad3
Miorel-Lucian Palii <mlpalii@gmail.com>
2010-02-08 12:16:35 -0400

    Deleted some unnecessary artifact files (because they have been reintegrated into the codebase elsewhere).
    git-svn-id: http://lawu.googlecode.com/svn@55 56f80a26-1f45-11de-82b5-194b17410922

--- trunk/src/lawu/lex/Lexer.java
+++
@@ -1,6 +0,0 @@
-package lawu.lex;
-
-import lawu.util.iterator.UniversalIterator;
-
-public interface Lexer<P extends TokenPattern> extends UniversalIterator<Token<P>> {
-}
--- trunk/util/scripts/changelog.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-svn2cl --group-by-day --linelen=80 --authors=authors.xml --break-before-msg --stdout | tee ChangeLog
